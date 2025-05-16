9b41bff86dea843c6ad69d8d39d07a7885434788
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2025-05-16 17:08:53 +0800

    Download passings

--- src/sysch_epub/convert.clj
+++ src/sysch_epub/convert.clj
@@ -8,6 +8,17 @@
    [selmer.parser :as selmer])
   (:gen-class))
 
+(def page-type-map
+  {"HEADER" :header
+   "TEXT" :text
+   "TEST" :test})
+
+(defn remap-page-type [page]
+  (assoc page :type (page-type-map (:type page))))
+
+(defn course-sections [course-meta]
+  (map remap-page-type (:sections course-meta)))
+
 (def course-root
   "https://aisystant.system-school.ru/api/courses/course-versions?course-path=")
 
@@ -17,12 +28,19 @@
       json/read-str
       :headers))
 
-(defn download-course-metadata [course-slug]
+(defn download-aisyst [url]
+  (:body
+   (http/get
+    url
+    {:headers (read-headers)})))
+
+(defn download-aisyst-json [url]
   (json/read-str
-   (:body
-    (http/get
-     (str course-root course-slug)
-     {:headers (read-headers)}))))
+   (download-aisyst url)))
+
+(defn download-course-metadata [course-slug]
+  (download-aisyst-json
+   (str course-root course-slug)))
 
 (defn -main [& args]
   (let [course-slug (first args)]
@@ -30,11 +48,22 @@
      (download-course-metadata course-slug))))
 
 (comment
+  (def passings
+    (download-aisyst-json "https://aisystant.system-school.ru/api/courses/courses-passing"))
+  ;https://aisystant.system-school.ru/api/courses/courses-passing
   (def course-meta
-    (download-course-metadata "ontologics-sobr"))
-  
+    (json/read-str
+     (slurp "resources/system-school/ontologics-sobr-2025-05-15.json")))
 
+  (def course-sections-clean
+    (course-sections course-meta))
+
+  (filter #(= :text (:type %)) course-sections-clean)
+
+  (filter #(and (not (:archived %)) (= "ontologics-sobr" (:coursePath %))) passings)
   (def latest-meta (last course-meta))
   (keys latest-meta)
   (:version latest-meta)
   :rcf)
+
+;https://aisystant.system-school.ru/api/courses/text/67669?course-passing=39713
